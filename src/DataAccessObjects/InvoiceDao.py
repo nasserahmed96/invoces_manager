0efa0469e2dcf443f8e6c28788f57c0c47b9fcb5
nasser <abdelnasserahmed96@gmail.com>
2022-01-27 11:44:31 +0200

    Added Python unittest instead of custom testing function

--- src/DataAccessObjects/InvoiceDao.py
+++ src/DataAccessObjects/InvoiceDao.py
@@ -1,4 +1,5 @@
 from src.DataAccessObjects.DataAccessObject import DataAccessObject
+from src.DataObjects.Invoice import Invoice
 
 
 class InvoiceDao(DataAccessObject):
@@ -13,7 +14,23 @@ class InvoiceDao(DataAccessObject):
         invoice_id = self.insert(values=invoice_values)
         return self.set_invoice_products(invoice_id, invoice.get_products()) if invoice_id > 0 else None
 
+    def get_invoice_by_serial_number(self, invoice_serial_number):
+        invoice_result = self.select(['id', 'serial_number', 'date'], [
+            {
+                'column': 'serial_number',
+                'value': invoice_serial_number,
+                'operator': '=',
+                'options': '',
+                'logic': '',
+            }
+        ])
+        if invoice_result and invoice_result.next():
+            return Invoice(invoice_id=invoice_result.value('id'),
+                           serial_number=invoice_result.value('serial_number'),
+                           date=invoice_result.value('date'))
+        return None
+
     def set_invoice_products(self, invoice_id, products):
-        self.insert([{'invoice_id': invoice_id, 'product_id': product} for product in products],
-                    table_name='invoices_products')
+        return invoice_id if self.insert([{'invoice_id': invoice_id, 'product_id': product} for product in products],
+                                         table_name='invoices_products') else -1
 
--- src/DataAccessObjectsTesting/InvoiceDaoTesting.py
+++ src/DataAccessObjectsTesting/InvoiceDaoTesting.py
@@ -5,35 +5,45 @@ import csv
 from src.DataObjects.Invoice import Invoice
 from src.DataObjects.Employee import Employee
 from src.DataObjects.Customer import Customer
+import unittest
 
 
-class InvoiceDaoTesting:
-    def __init__(self):
-        self.FILE_PATH = config.TEST_DATA_PATH + 'invoices_data_testing.csv'
-        self.invoice_dao = InvoiceDao()
-
+class InvoiceDaoTesting(unittest.TestCase):
     def read_file(self):
         with open(self.FILE_PATH) as data_file:
             return list(csv.DictReader(data_file))
 
     def test_create_invoice(self):
+        self.FILE_PATH = config.TEST_DATA_PATH + 'invoices_data_testing.csv'
+        self.invoice_dao = InvoiceDao()
         invoices = self.read_file()
         for invoice in invoices:
             invoice['products'] = invoice['products'].split('|')
         print('Invoices testing data: ', invoices)
         for invoice in invoices:
-            print(self.invoice_dao.create_invoice(Invoice(serial_number=invoice['serial_number'],
+            new_invoice = self.invoice_dao.create_invoice(Invoice(serial_number=invoice['serial_number'],
                                                           date=invoice['date'],
                                                           employee=Employee(id=invoice['employee_id']),
                                                           customer=Customer(id=invoice['customer_id']),
-                                                          products=invoice['products'])))
+                                                          products=invoice['products']))
+            print('New invoice: ', new_invoice)
+            self.assertFalse(new_invoice is None)
+            self.assertGreater(new_invoice, 0)
 
-    def run_tests(self):
-        self.test_create_invoice()
+
+    def test_get_invoice_by_serial_number(self):
+        self.invoice_dao = InvoiceDao()
+        invoice_id = 13
+        serial_number = '231564897222'
+        invoice_date = '2022-02-08'
+        invoice = self.invoice_dao.get_invoice_by_serial_number(serial_number)
+        self.assertTrue(isinstance(invoice, Invoice))
+        self.assertEqual(invoice.get_invoice_id(), invoice_id)
+        self.assertEqual(invoice.get_serial_number(), serial_number)
+        self.assertEqual(invoice.get_date(), invoice_date)
 
 
 if __name__ == '__main__':
-    invoice_testing = InvoiceDaoTesting()
-    invoice_testing.run_tests()
+    unittest.main()
 
 
