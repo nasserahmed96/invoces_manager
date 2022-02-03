fef5d2893fa64b76b913289e5e24bf341faf7b84
nasser <abdelnasserahmed96@gmail.com>
2022-02-03 08:51:33 +0200

    Edit Database default name to handle windows environment

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import re
 
 
 PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 TEST_DATA_PATH = PROJECT_ROOT_PATH + '/Testing_Data/'
-PROJECT_NAME = PROJECT_ROOT_PATH.split('/')[-1]
+#Split bases on \ or / for Unix and windows
+PROJECT_NAME = re.split(r'\/|\\', PROJECT_ROOT_PATH)[-1]
 REPORTS_ROOT_PATH = PROJECT_ROOT_PATH + '/src/Reports/'
 
 sys.path.insert(0, PROJECT_ROOT_PATH)
